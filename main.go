b9fc0a97575e0c8079fd9acfad17728c10c762f6
Cayo Hollanda <admin@example.com>
2018-11-29 12:29:17 -0200

    initial commit

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/url"
+	"os"
+	"time"
+
+	"github.com/Azure/azure-storage-blob-go/azblob"
+
+	"./utils"
+)
+
+func main() {
+	utils.TimedPrintln("Iniciando rotina...")
+
+	var (
+		azureAccount      = os.Getenv("AZURE_STORAGE_ACCOUNT")
+		azureAccessKey    = os.Getenv("AZURE_STORAGE_ACCESS_KEY")
+		containerName     = os.Getenv("AZURE_STORAGE_CONTAINER_NAME")
+		directoryToUpload = os.Getenv("AZURE_DIRECTORY_TO_UPLOAD")
+	)
+
+	if len(azureAccount) == 0 || len(azureAccessKey) == 0 || len(containerName) == 0 || len(directoryToUpload) == 0 {
+		utils.TimedPrintln("Defina as variáveis de ambiente: AZURE_STORAGE_ACCOUNT, AZURE_STORAGE_ACCESS_KEY e AZURE_STORAGE_CONTAINER_NAME, AZURE_DIRECTORY_TO_UPLOAD")
+		os.Exit(1)
+	}
+
+	utils.TimedPrintln("Setando as credenciaiss...")
+	credentials, err := azblob.NewSharedKeyCredential(azureAccount, azureAccessKey)
+	utils.CheckErr("Erro com as credenciais informadas", err)
+	utils.TimedPrintln("Credenciais setadas")
+
+	pipeline := azblob.NewPipeline(credentials, azblob.PipelineOptions{})
+
+	URL, _ := url.Parse(fmt.Sprintf("https://%s.blob.core.windows.net/%s", azureAccount, containerName))
+
+	containerURL := azblob.NewContainerURL(*URL, pipeline)
+
+	context := context.Background()
+
+	utils.TimedPrintln("Zipando arquivo...")
+	timeBlob := time.Now()
+	zipFilename := fmt.Sprintf("blob%d%d%d%02d%02d%02d", timeBlob.Day(), timeBlob.Month(), timeBlob.Year(), timeBlob.Hour(), timeBlob.Minute(), timeBlob.Second())
+	utils.ZipWriter(directoryToUpload, zipFilename) // criando arquivo zip
+
+	blobURL := containerURL.NewBlockBlobURL(zipFilename)
+	file, err := os.Open(zipFilename)
+	utils.CheckErr("Erro ao verificar se o arquivo zip existe", err)
+
+	utils.TimedPrintln("Fazendo upload do blob para a Azure...")
+	_, err = azblob.UploadFileToBlockBlob(context, file, blobURL, azblob.UploadToBlockBlobOptions{})
+	utils.CheckErr("Erro ao fazer upload do blob", err)
+	utils.TimedPrintln("Upload feito com sucesso!")
+
+	utils.TimedPrintln("Fazendo download do blob...")
+	_, err = blobURL.Download(context, 0, azblob.CountToEnd, azblob.BlobAccessConditions{}, false)
+	utils.CheckErr("", err)
+	utils.TimedPrintln("Download feito com sucesso")
+
+	os.Remove(zipFilename)
+}
---
+++ utils/utils.go
@@ -0,0 +1,77 @@
+package utils
+
+import (
+	"archive/zip"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"time"
+)
+
+// ZipWriter is a function to zip directory
+func ZipWriter(path string, filename string) {
+	baseFolder := path
+
+	// Get a Buffer to Write To
+	outFile, err := os.Create(filename)
+	CheckErr("", err)
+	defer outFile.Close()
+
+	// Create a new zip archive.
+	w := zip.NewWriter(outFile)
+
+	// Add some files to the archive.
+	AddFiles(w, baseFolder, "")
+
+	CheckErr("", err)
+
+	// Make sure to check the error on Close.
+	err = w.Close()
+	CheckErr("", err)
+}
+
+// AddFiles is a function to add files to zip file
+func AddFiles(w *zip.Writer, basePath, baseInZip string) {
+	// Open the Directory
+	files, err := ioutil.ReadDir(basePath)
+	CheckErr("", err)
+
+	for _, file := range files {
+		TimedPrintln("Adicionando arquivo: " + basePath + file.Name())
+		if !file.IsDir() {
+			dat, err := ioutil.ReadFile(basePath + file.Name())
+			CheckErr("", err)
+
+			// Add some files to the archive.
+			f, err := w.Create(baseInZip + file.Name())
+			CheckErr("", err)
+
+			_, err = f.Write(dat)
+			CheckErr("", err)
+		} else if file.IsDir() {
+
+			// Recurse
+			newBase := basePath + file.Name() + "/"
+			TimedPrintln("Recursing and Adding SubDir: " + file.Name())
+			TimedPrintln("Recursing and Adding SubDir: " + newBase)
+
+			AddFiles(w, newBase, file.Name()+"/")
+		}
+	}
+}
+
+// TimedPrintln is a function to print a message with the time
+func TimedPrintln(message string) {
+	t := time.Now()
+
+	fmt.Printf("[%d/%d/%d às %02d:%02d:%02d] %s\n", t.Day(), t.Month(), t.Year(), t.Hour(), t.Minute(), t.Second(), message)
+}
+
+// CheckErr is a function to handle the erros of application
+func CheckErr(message string, err error) {
+	if err != nil {
+		TimedPrintln(message)
+		fmt.Printf("%+v", err)
+		panic(err)
+	}
+}
